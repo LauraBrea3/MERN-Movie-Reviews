aa4de9edf75a5374c6efedfb918e8759e963f88a
Laura Brea <laurabrea17@gmail.com>
2023-04-25 21:41:54 -0400

    connected DB sucessfully

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-//const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
 app.get('*', (req,res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
+
+mongoose.set('strictQuery', true)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
 
 const PORT = process.env.PORT
 
